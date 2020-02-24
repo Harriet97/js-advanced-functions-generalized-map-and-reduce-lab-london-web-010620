ff95c51644b0e8afb301daf121b9f657f5441e3b
Harriet Bicknell <harrietindubai@gmail.com>
2020-02-24 16:22:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+const map = (sourceArray, func) => {
+    return sourceArray.map(element => func(element));
+  };
+
+  const reduce = (sourceArray, func, startingValue = 0) => {
+    if (startingValue) {
+    return sourceArray.reduce(func, startingValue)
+    } else {
+        return sourceArray.reduce(func)
+    };
+  };
+
+  
+ 
